74ba321cec26b2ad6b33e8d2951ca59f15223bd8
Creepydncer <niltonbiju@gmail.com>
2026-02-08 22:57:59 -0300

    Mudança no sobre

--- src/config.jsx
+++ src/config.jsx
@@ -12,11 +12,11 @@ export const siteConfig = {
     tagline: "Uma psi tão humana quanto você ",
     
     // SOBRE (edite aqui o texto completo)
-    about: `Sou psicóloga clínica com especialização em Neuropsicologia e Terapia Cognitivo Comportamental (TCC). 
+    about: `Sou psicóloga em Caçador/SC, atuo com a Terapia Cognitivo-Comportamental (TCC) e sou especialista em Neuropsicologia. 
     
     Acredito em um atendimento humanizado, onde cada paciente é acolhido em suas singularidades. Meu trabalho é baseado em evidências científicas, sempre respeitando o tempo e o espaço de cada pessoa.
     
-    Atendo adolescentes, adultos e idosos, tanto presencialmente quanto online, oferecendo um ambiente seguro e acolhedor para que você possa cuidar da sua saúde mental.`,
+    Atendo adolescentes, adultos e idosos, tanto presencialmente quanto  terapia online, oferecendo um ambiente seguro e acolhedor para que você possa cuidar da sua saúde mental.`,
   },
 
   // CONTATO
